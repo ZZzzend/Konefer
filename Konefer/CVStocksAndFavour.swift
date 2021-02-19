830fcb7f9cad4f9f0c9379f15cd6eca77851fddb
Владислав <Vladislav@MacBook-Pro-Temp.local>
2021-02-19 14:57:37 +0800

    Added searchBar and CollectionView

---
+++ Konefer/CVStocksAndFavour.swift
@@ -0,0 +1,17 @@
+//
+//  CVStocksAndFavour.swift
+//  Konefer
+//
+//  Created by Владислав on 19.02.2021.
+//
+
+import UIKit
+
+class CVStocksAndFavour: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
